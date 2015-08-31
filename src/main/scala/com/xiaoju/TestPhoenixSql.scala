7526825b5a803d0c1bcca792f7a269e8849088d5
cenyuhai <cenyuhai@diditaxi.com.cn>
2015-08-31 20:23:13 +0800

    上传并发测试sql的例子

--- src/main/scala/com/xiaoju/TestPhoenixSql.scala
+++ src/main/scala/com/xiaoju/TestPhoenixSql.scala
@@ -12,30 +12,32 @@ import scala.concurrent.duration._
 object TestPhoenixSql {
 
   def testParalism(sql: String, paralism: Int): Unit = {
-    val futureList = Future.sequence((1 to paralism).map(x=>
-      Future[Long] {
-        var start = System.currentTimeMillis()
-        var conn = DriverManager.getConnection("jdbc:phoenix:localhost:2181")
-        var end = System.currentTimeMillis()
-        println("connect to server use " + (end - start) + " ms")
-        start = end
-        var statement = conn.createStatement()
-        end = System.currentTimeMillis()
-        println("create statment use " + (end - start) + " ms")
-        start = end
-        statement.executeQuery(sql)
-        println("execute query use " + (end - start) + " ms")
-        statement.close()
-        conn.close()
-        return (end - start)
+    var startTime = System.currentTimeMillis()
+    var threads = new Array[Thread](paralism)
+    (0 to paralism).foreach { i=>
+      threads(i) = new Thread() {
+        override def run(): Unit = {
+          var start = System.currentTimeMillis()
+          var conn = DriverManager.getConnection("jdbc:phoenix:localhost:2181")
+          var end = System.currentTimeMillis()
+          println("connect to server use " + (end - start) + " ms")
+          start = end
+          var statement = conn.createStatement()
+          end = System.currentTimeMillis()
+          println("create statment use " + (end - start) + " ms")
+          start = end
+          statement.executeQuery(sql)
+          println("execute query use " + (end - start) + " ms")
+          statement.close()
+          conn.close()
+        }
       }
-    ).toList)
-    var sum = 0L
-    val oddSum = futureList.map(_.sum)
-    oddSum.foreach { x=>
-      sum  = sum + x
     }
-    println("totally use " + sum + " ms")
+    threads.foreach(_.start())
+    threads.foreach(_.join())
+    var endTime = System.currentTimeMillis()
+
+    println("totally use " + (endTime - startTime)  + " ms")
   }
 
   def main(args:Array[String]): Unit = {
