c5250fee4aafd2ae7101743421ccfbef49a4b816
cenyuhai <cenyuhai@didichuxing.com>
2015-11-24 21:29:59 +0800

    设置输入目录

--- src/main/java/org/apache/hadoop/hbase/regionserver/HFileScanMapreduce.java
+++ src/main/java/org/apache/hadoop/hbase/regionserver/HFileScanMapreduce.java
@@ -22,6 +22,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
 
@@ -256,7 +257,7 @@ public class HFileScanMapreduce {
       job.setOutputFormatClass(OrcNewOutputFormat.class);
       job.setMapOutputKeyClass(NullWritable.class);
       job.setMapOutputValueClass(Writable.class);
-
+      FileInputFormat.setInputPaths(job, new Path(tableDir, "tmp"));
       FileOutputFormat.setOutputPath(job, new Path(tableDir, "tmp"));
       System.exit(job.waitForCompletion(true) ? 0 : 1);
 
